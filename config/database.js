61042bc82f31321c7100e732fb420942ac9a50bd
Lis Pacheco <lmpacheco@uce.edu.ec>
2025-06-22 17:59:33 -0500

    chore: add Dockerfile and .dockerignore to containerize

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'user_service_db',
-  process.env.DB_USER || 'admin',
-  process.env.DB_PASSWORD || 'PoolG10.11',
+  'user_service_db',              
+  'admin',                 
+  'PoolG10.11',    
   {
-    host: process.env.DB_HOST || 'mysql.cwizczqy6pft.us-east-1.rds.amazonaws.com',
+    host: 'mysql.cwizczqy6pft.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     logging: false,
   }
